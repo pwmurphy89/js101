26574377ff0301d75fa6b074e04978e45d0699c4
pwmurphy89 <pwmurphy89@gmail.com>
2016-03-04 10:00:38 -0500

    Some changes

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+getElementById("message").alert("There's a dragon!");
+
+function throwDice() {
+
+
+	var randomDie = Math.round(Math.random()*5) + 1;
+
+	var imageName = "dice-img/d" + randomDie + ".gif";
+	console.log(imageName);
+	document.images['myDie1'].src = imageName;
+
+	var randomDie2 = Math.round(Math.random() * 5) + 1;
+	var imageName2 = "dice-img/d" + randomDie2 + ".gif";
+	document.images['myDie2'].src = imageName2;
+	console.dir(document.images);
+var dieTotal = randomDie + randomDie2;
+
+	
+
+}
+
+
+function run() {
+	document.getElementById("dragon").style.visibility = "hidden";
+	document.getElementById("fleeing").style.visibility = "visible";
+	alert("You sceered!");
+}
+
+
